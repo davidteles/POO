83bbafff80190d3fc4ed33832daa92bc29a1169c
David Teles <davidteles@David-Teles.local>
2018-05-02 15:59:58 +0100

    Corrected

--- Event.java
+++ Event.java
@@ -16,7 +16,7 @@ public abstract class Event {
 		Move move = new Move(1,2);
 		Death death = new Death(1,3);
 		Reproduction reproduction = new Reproduction(1,4);
-		Epidemic epidemic = new Epidemic(5);
+
 		System.out.println("Individual "+ move.ID +" will move in instant "+ move.instant + " to the variable "+ move.direction+".");
 		System.out.println("Individual "+ death.ID +" will die in instant "+ death.instant+".");
 		System.out.println("Individual "+ reproduction.ID +" will reproduce in instant "+ reproduction.instant + ".");
