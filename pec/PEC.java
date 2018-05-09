6eb9b3f576ea7c02cd77c69fe5d5388d793a2f52
armcoias <30505058+armcoias@users.noreply.github.com>
2018-05-09 18:38:59 +0100

    Add files via upload

--- pec/PEC.java
+++ pec/PEC.java
@@ -1,5 +1,6 @@
 package pec;
 
+//Importacao dos packages necessarios
 import java.util.PriorityQueue;
 
 import event.Event;
@@ -7,33 +8,34 @@ import event.Event;
 import java.util.Comparator; 
 
 
-
 public class PEC {
 	 
-	 PriorityQueue<Event> pec;
-	 
+	//FIELD
+	 PriorityQueue<Event> pec; /*Fila de prioridades para organizar os eventos de acordo 
+	 						com o instante em que devem ocorrer*/
+	 //CONSTRUCTOR
 	 public PEC() {
-		 
-		 pec = new PriorityQueue<Event>( new Checker()); 
+		 pec = new PriorityQueue<Event>( new Checker()); //Cria a fila de prioridades
 	 }
 
+	 //METODOS
+	 
+	 //Adiciona evento a fila
 	 public void addToPEC(Event aux) {
 		 this.pec.add(aux);
 	 }
 	 
-
+	 //Retira da fila o evento mais prioritario
 	 public Event getNextEvent() {
 			
 			return this.pec.poll();
 		}
 	 
-
-	 
-
-	 
-	 
 }
 
+//INTERFACES
+
+//Interface para estabelecer a prioridade dos eventos
 class Checker implements Comparator<Event>{
 	public int compare(Event a, Event b)
     {
