6bec66b0131b89fb5764a9383240cbf9e26f624d
duartebfernandes <38788115+duartebfernandes@users.noreply.github.com>
2018-05-10 18:24:19 +0100

    Update Population.java

--- project/Population.java
+++ project/Population.java
@@ -1,22 +1,22 @@
 package project;
 
-//Importacao do packages necessarios
+/*Importacao do packages necessarios*/
 import java.util.LinkedList;
 
 public class Population {
 	
-	//FIELDS
-	protected int v; //populacao atual - numero atual de individuos - populacao inicial recebida do ficheiro
-	private int v_max; //numero max de individuos que pode existir - recebido do ficheiro 
-	protected Coord init_pos; //coordenadas da posicao inicial - recebido do ficheiro 
-	protected Coord fin_pos; //coordenadas da posicao final - recebido do ficheiro 
-	protected int k; //sensibilidade do conforto a pequenas variacoes - recebido do ficheiro 
-	protected int d_param; //parametro de morte - recebido do ficheiro 
-	protected int r_param; //parametro de reproducao - recebido do ficheiro 
-	protected int m_param; //parametro de morte - recebido do ficheiro 
-	LinkedList<Individual> individuals; //lista de individuos existentes
+	/*FIELDS*/
+	protected int v; /*populacao atual - numero atual de individuos - populacao inicial recebida do ficheiro*/
+	private int v_max; /*numero max de individuos que pode existir - recebido do ficheiro*/ 
+	protected Coord init_pos; /*coordenadas da posicao inicial - recebido do ficheiro*/ 
+	protected Coord fin_pos; /*coordenadas da posicao final - recebido do ficheiro*/ 
+	protected int k; /*sensibilidade do conforto a pequenas variacoes - recebido do ficheiro*/ 
+	protected int d_param; /*parametro de morte - recebido do ficheiro*/ 
+	protected int r_param; /*parametro de reproducao - recebido do ficheiro*/ 
+	protected int m_param; /*parametro de morte - recebido do ficheiro*/ 
+	LinkedList<Individual> individuals; /*lista de individuos existentes*/
 	
-	//CONSTRUCTOR
+	/*CONSTRUCTOR*/
 	public Population(int vact, int vmax, Coord ini, Coord fin, int sens, int dp, int rp, int mp) {
 		this.v = vact; 
 		this.v_max = vmax; 
@@ -29,24 +29,24 @@ public class Population {
 		this.individuals = new LinkedList<Individual>();
 	}
 	
-	//METODOS
+	/*METODOS*/
 	
-	//Get do numero maximo de individuos que podem existir
+	/*Get do numero maximo de individuos que podem existir*/
 	public int getV_max() {
 		return v_max;
 	}
 	
-	//Set do numero maximo de individuos que podem existir
+	/*Set do numero maximo de individuos que podem existir*/
 	public void setV_max(int v_max) {
 		this.v_max = v_max;
 	}
 	
-	//Set do numero de individuos existentes
+	/*Set do numero de individuos existentes*/
 	public void setV(int v) {
 		this.v = v;
 	}
 	
-	//Preenche a lista de indiviuos com todos os individuos existentes no inicio
+	/*Preenche a lista de indiviuos com todos os individuos existentes no inicio*/
 	public void addIndividuals(int v, Coord pos, int cmax, int n, int m) {
 			
 		while(this.individuals.size() != v) {
@@ -66,7 +66,7 @@ public class Population {
 	}
 	
 
-	//Funcao que procura o individio com um determinado ID
+	/*Funcao que procura o individio com um determinado ID*/
 	public Individual findID(int id) {
 		for(int aux = 0; aux<this.individuals.size() ; aux++) {
 			if(this.individuals.get(aux).getId()==id) {
